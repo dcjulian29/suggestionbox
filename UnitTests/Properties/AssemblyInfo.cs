d783476f462c8f920351aa02b499267b9ed88d5e
Julian Easterling <julian@julianscorner.com>
2014-11-14 18:25:36 -0500

    Added Unit Tests Project

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("08e9cf20-8aa8-4894-9289-4062f270fb36")]
