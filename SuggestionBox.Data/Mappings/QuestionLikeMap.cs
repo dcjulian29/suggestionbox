cf45664f87b59e298d569895038289a19a470260
Julian Easterling <julian@julianscorner.com>
2014-12-11 09:06:13 -0500

    Added mapping for Question Likes

---
+++ SuggestionBox.Data/Mappings/QuestionLikeMap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using SuggestionBox.Data.Entities;
+
+namespace SuggestionBox.Data.Mappings
+{
+    /// <summary>
+    /// QuestionLike NHibernate Mapping
+    /// </summary>
+    public class QuestionLikeMap : ClassMap<QuestionLike>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QuestionLikeMap"/> class.
+        /// </summary>
+        public QuestionLikeMap()
+        {
+            Table("QuestionLikes");
+
+            Id(x => x.Id);
+
+            Map(x => x.HashedIpAddress);
+            Map(x => x.LastTimeLiked);
+            Map(x => x.QuestionId);
+        }
+    }
+}
