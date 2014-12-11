5fb2076886d627d207bfc9907679adc38b5e63f4
Julian Easterling <julian@julianscorner.com>
2014-12-11 08:51:05 -0500

    Added a mapping class for Home Page Visits

---
+++ SuggestionBox.Data/Mappings/HomePageVisitMap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using SuggestionBox.Data.Entities;
+
+namespace SuggestionBox.Data.Mappings
+{
+    /// <summary>
+    /// HomePageVisit NHibernate Mapping
+    /// </summary>
+    public class HomePageVisitMap : ClassMap<HomePageVisit>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HomePageVisitMap"/> class.
+        /// </summary>
+        public HomePageVisitMap()
+        {
+            Table("HomePageVisits");
+
+            Id(x => x.Id);
+
+            Map(x => x.HashedIpAddress);
+            Map(x => x.LastVisit);
+            Map(x => x.NumberOfVisits);
+        }
+    }
+}
