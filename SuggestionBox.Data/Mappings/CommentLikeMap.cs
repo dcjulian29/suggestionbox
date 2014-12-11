24cdbc65d04dfcea799620a8dab0ed0f2c83076d
Julian Easterling <julian@julianscorner.com>
2014-12-11 08:49:03 -0500

    Added mapping class for CommentLikes

---
+++ SuggestionBox.Data/Mappings/CommentLikeMap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using SuggestionBox.Data.Entities;
+
+namespace SuggestionBox.Data.Mappings
+{
+    /// <summary>
+    /// CommentLike NHibernate Mapping
+    /// </summary>
+    public class CommentLikeMap : ClassMap<CommentLike>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommentLikeMap"/> class.
+        /// </summary>
+        public CommentLikeMap()
+        {
+            Table("CommentLikes");
+
+            Id(x => x.Id);
+
+            Map(x => x.HashedIpAddress);
+            Map(x => x.LastTimeLiked);
+            Map(x => x.CommentId);
+        }
+    }
+}
