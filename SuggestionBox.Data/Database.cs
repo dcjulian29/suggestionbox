af3ecc3f0ce7d650ce892a6a011b975d4f084143
Julian Easterling <julian@julianscorner.com>
2015-01-26 00:09:13 -0500

    Added Database Class that will provide the implementation session factory class

---
+++ SuggestionBox.Data/Database.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolKit.Data.NHibernate;
+using ToolKit.Data.NHibernate.SessionFactories;
+
+namespace SuggestionBox.Data
+{
+    /// <summary>
+    /// Provides Session Managers for the various databases that this applications uses.
+    /// </summary>
+    public static class Database
+    {
+        /// <summary>
+        /// Gets the session manager for the Feedback database.
+        /// </summary>
+        public static SessionManager Feedback
+        {
+            get
+            {
+                return new SessionManager(new SqLiteSessionFactory("db_FEEDBACK"));
+            }
+        }
+    }
+}
