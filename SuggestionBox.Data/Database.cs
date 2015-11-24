512bf14b42b15b988dd8d94f8cd3adc435634a67
Julian Easterling <julian@julianscorner.com>
2015-11-23 20:32:29 -0500

    updated using statements

--- SuggestionBox.Data/Database.cs
+++ SuggestionBox.Data/Database.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ToolKit.Data.NHibernate;
+﻿using ToolKit.Data.NHibernate;
 using ToolKit.Data.NHibernate.SessionFactories;
 
 namespace SuggestionBox.Data
