41606cb550c39d6a77fe88daac11b6e2498ee2c7
Julian Easterling <julian@julianscorner.com>
2019-11-16 11:14:13 -0500

    Updated to .Net Core 2.2

---
+++ SuggestionBox/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SuggestionBox.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
