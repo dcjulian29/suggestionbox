9f1466bf25c0a2b521ad7daf5660a700f73fa79f
Julian Easterling <julian@julianscorner.com>
2020-03-09 17:24:31 -0400

    Added Unit test for Question likes

--- SuggestionBox.Data/Entities/QuestionLike.cs
+++ SuggestionBox.Data/Entities/QuestionLike.cs
@@ -4,32 +4,41 @@ using ToolKit.Data;
 namespace SuggestionBox.Data.Entities
 {
     /// <summary>
-    ///     Question-Like Entity
+    ///   Question-Like Entity
     /// </summary>
     public class QuestionLike : Entity
     {
         /// <summary>
-        ///     Gets or sets the hashed IP address of the visitor.
+        ///   Initializes a new instance of the <see cref="QuestionLike" /> class.
+        /// </summary>
+        public QuestionLike()
+        {
+            HashedIpAddress = String.Empty;
+            QuestionId = 0;
+        }
+
+        /// <summary>
+        ///   Gets or sets the hashed IP address of the visitor.
         /// </summary>
         /// <remarks>
-        ///     Only store the hashed IP address to prevent the ability to trace back to the
-        ///     original IP Address.
+        ///   Only store the hashed IP address to prevent the ability to trace back to the original
+        ///   IP Address.
         /// </remarks>
         public virtual string HashedIpAddress { get; set; }
 
         /// <summary>
-        ///     Gets or sets the last time this question was liked.
+        ///   Gets or sets the last time this question was liked.
         /// </summary>
         public virtual DateTime LastTimeLiked { get; set; }
 
         /// <summary>
-        ///     Gets or sets the ID of the question liked.
+        ///   Gets or sets the ID of the question liked.
         /// </summary>
         public virtual int QuestionId { get; set; }
 
         /// <summary>
-        ///     Determine if we should accept the like. If the last time this visitor liked this
-        ///     question was less than 10 minute (600 seconds), reject the like.
+        ///   Determine if we should accept the like. If the last time this visitor liked this
+        ///   question was less than 10 minute (600 seconds), reject the like.
         /// </summary>
         /// <returns><c>true</c> if the caller should accept the like; otherwise, <c>false</c>.</returns>
         public virtual bool AcceptLike()
---
+++ UnitTests/Data/QuestionLikeTests.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using SuggestionBox.Data.Entities;
+using Xunit;
+
+namespace UnitTests.Data
+{
+    [SuppressMessage(
+        "StyleCop.CSharp.DocumentationRules",
+        "SA1600:ElementsMustBeDocumented",
+        Justification = "Test Suites do not need XML Documentation.")]
+    public class QuestionLikeTests
+    {
+        [Fact]
+        public void AcceptLike_Should_AcceptALike()
+        {
+            // Arrange
+            var now = DateTime.UtcNow;
+            var like = new QuestionLike();
+
+            // Act
+            _ = like.AcceptLike();
+
+            // Assert
+            Assert.True(now < like.LastTimeLiked);
+        }
+
+        [Fact]
+        public void AcceptLike_Should_RejectRepeatedLikes()
+        {
+            // Arrange
+            DateTime now;
+            var like = new QuestionLike();
+
+            // Act
+            _ = like.AcceptLike();
+            now = DateTime.UtcNow;
+            _ = like.AcceptLike();
+
+            // Assert
+            Assert.True(now > like.LastTimeLiked);
+        }
+
+        [Fact]
+        public void AcceptLike_Should_ReturnFalse_When_RepeatedLikes()
+        {
+            // Arrange
+            var like = new QuestionLike();
+
+            // Act
+            _ = like.AcceptLike();
+            var actual = like.AcceptLike();
+
+            // Assert
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void AcceptLike_Should_ReturnTrue()
+        {
+            // Arrange
+            var like = new QuestionLike();
+
+            // Act
+            var actual = like.AcceptLike();
+
+            // Assert
+            Assert.True(actual);
+        }
+    }
+}
